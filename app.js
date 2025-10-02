6f64d3191b822e239b432075398ebc2fc59d6fe6
gabisandova@uc.cl <yonatan>
2025-10-02 01:25:13 -0300

    change limit

--- app.js
+++ app.js
@@ -11,16 +11,16 @@ app.use(express.json());
 // CORS ZONE
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "*");
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, PATCH, DELETE, OPTIONS"
-  );
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    res.header(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PUT, PATCH, DELETE, OPTIONS"
+    );
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(200);
+    }
+    next();
 });
 
 // RATE LIMIT ZONE
@@ -28,19 +28,19 @@ app.use((req, res, next) => {
 const { rateLimit, ipKeyGenerator } = require("express-rate-limit"); // importar helper
 
 const limiter = rateLimit({
-  windowMs: 2 * 60 * 1000,
-  limit: 20,
-  message: {
-    error: "Demasiadas solicitudes con esta API-Key, intenta más tarde",
-  },
-  keyGenerator: (req, res) => {
-    const apikey = req.header("Authorization");
-    if (apikey) {
-      return String(apikey);
-    }
+    windowMs: 2 * 60 * 1000,
+    limit: 50,
+    message: {
+        error: "Demasiadas solicitudes con esta API-Key, intenta más tarde",
+    },
+    keyGenerator: (req, res) => {
+        const apikey = req.header("Authorization");
+        if (apikey) {
+            return String(apikey);
+        }
 
-    return ipKeyGenerator(req);
-  },
+        return ipKeyGenerator(req);
+    },
 });
 app.use(limiter);
 
@@ -54,5 +54,5 @@ app.use(limiter, putRouter);
 app.use(limiter, deleteRouter);
 
 app.listen(3000, () => {
-  console.log("Servidor corriendo en http://localhost:3000");
+    console.log("Servidor corriendo en http://localhost:3000");
 });
